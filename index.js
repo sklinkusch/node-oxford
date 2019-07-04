f0f7ab2592cfd802f6d1737b5bee98a8dc205fe9
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-07-04 20:27:47 +0200

    output is correct

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-const axios = require('axios');
+const axios = require("axios");
 
 const myArgs = process.argv.slice(2);
-const language = myArgs[0];
-const word = myArgs[1];
+const language = "en-gb";
+const word = myArgs[0];
 const id = "2c7f463b";
 const keys = "3bdb259dec0727a36ca851467148a1e5";
 const url = `https://od-api.oxforddictionaries.com:443/api/v2/entries/${language}/${word.toLowerCase()}`;
 getData(url, id, keys);
 
-
 async function getData(url, id, keys) {
   const response = await axios(url, { headers: { app_id: id, app_key: keys } });
   const { data } = await response;
-  const { results } = await data;
-  results.forEach((result, inda) => {
+  const { results, word: myWord } = await data;
+  results.forEach(result => {
     const { lexicalEntries } = result;
-    lexicalEntries.forEach((entry, indb) => {
-      const { text, entries, lexicalCategory } = entry;
-      entries.forEach((entr, indc) => {
+    lexicalEntries.forEach(entry => {
+      const { entries, lexicalCategory } = entry;
+      const { text: wordtype } = lexicalCategory;
+      console.log(`${myWord} (${wordtype})`);
+      entries.forEach(entr => {
         const { senses } = entr;
         senses.forEach(sense => {
           const { shortDefinitions } = sense;
-          console.log(shortDefinitions);
+          shortDefinitions.forEach((definition, index) => {
+            console.log(`${index + 1}. ${definition}`);
+          });
         });
       });
     });
