753e388e812b15fe4c9f8171546b1aa41db3c7c2
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-07-04 21:00:42 +0200

    commented and shortened client.js

--- client.js
+++ client.js
@@ -1,20 +1,26 @@
 const axios = require("axios");
 
 exports.getData = async (url, id, keys) => {
+  // axios request -> response
   const response = await axios(url, { headers: { app_id: id, app_key: keys } });
-  const { data } = await response;
-  const { results, word: myWord } = await data;
+  // destructuring important data from response
+  const {
+    data: { results, word: myWord }
+  } = await response;
+  // moving in the stack of forEach loops
   results.forEach(result => {
-    const { lexicalEntries } = result;
-    lexicalEntries.forEach(entry => {
-      const { entries, lexicalCategory } = entry;
-      const { text: wordtype } = lexicalCategory;
+    result.lexicalEntries.forEach(entry => {
+      // double destructuring
+      const {
+        entries,
+        lexicalCategory: { text: wordtype }
+      } = entry;
+      // console.log of word and word type
       console.log(`${myWord} (${wordtype})`);
-      entries.forEach(entr => {
-        const { senses } = entr;
-        senses.forEach(sense => {
-          const { shortDefinitions } = sense;
-          shortDefinitions.forEach((definition, index) => {
+      entries.forEach(item => {
+        item.senses.forEach(sense => {
+          sense.shortDefinitions.forEach((definition, index) => {
+            // console.log of list of definitions
             console.log(`${index + 1}. ${definition}`);
           });
         });
